fdc038803b78bd3b9c9624ab81e8a717369df883
FIFIT <fifitt@gmail.com>
2022-10-31 09:56:40 +0700

    first commit

---
+++ app/src/main/java/com/example/fifitjobsheet15/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.fifitjobsheet15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"aplikasi berjalan" , Toast.LENGTH_SHORT).show()
+
+        val tombol = findViewById<Button>(R.id.tombolToast)
+
+        tombol.setOnClickListener {
+            Toast.makeText(this, "Fifit Sholikhatul Mufalah", Toast.LENGTH_SHORT).show()
+        }
+        val tombolkls = findViewById<Button>(R.id.tombolKELAS)
+
+        tombolkls.setOnClickListener {
+            Toast.makeText(this, "XI PPLG 1", Toast.LENGTH_SHORT).show()
+        }
+        val tombolnis = findViewById<Button>(R.id.tombolNIS)
+        tombolnis.setOnClickListener {
+            Toast.makeText(this, "2989", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
